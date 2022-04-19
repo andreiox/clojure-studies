972e6743de1d2f10fa7e7af2fa5d7246208d7b15
Andre Macedo <dee.xd@hotmail.com>
2022-04-19 15:11:17 -0300

    feat: add schema class

---
+++ alura/alura-schemas/hospital/src/hospital/schemas.clj
@@ -0,0 +1,47 @@
+(ns hospital.schemas
+  (:use clojure.pprint)
+  (:require [schema.core :as s]))
+
+(println (s/validate Long 15))
+
+(defn print-patient-id
+  [patient]
+  (println (:id patient)))
+
+(defn test-print-patient-id
+  []
+  (let [patient1 {:id 1, :name "Andre"}
+        patient2 {:name "Eduarda"}]
+    (print-patient-id patient1)
+    (print-patient-id patient2)))
+
+(test-print-patient-id)
+
+
+(s/set-fn-validation! true)
+
+(s/defn simple-test
+  [number :- Long]
+  (println number))
+
+(simple-test 18)
+(simple-test "Andre")
+
+
+(def Patient
+  "Patient schema"
+  {:id Long
+   :name String})
+
+(s/defn print-patient-id-with-validation
+  [patient :- Patient]
+  (println (:id patient)))
+
+(defn test-print-patient-id-with-validation
+  []
+  (let [patient1 {:id 1, :name "Andre"}
+        patient2 {:name "Eduarda"}]
+    (print-patient-id-with-validation patient1)
+    (print-patient-id-with-validation patient2)))
+
+(test-print-patient-id-with-validation)
