aff24ae8ad43a42cc5b90a47705f7758cabfbe93
Andre Macedo <dee.xd@hotmail.com>
2022-04-22 16:59:48 -0300

    feat: add generators example

--- alura/alura-tests/hospital/src/hospital/core.clj
+++ alura/alura-tests/hospital/src/hospital/core.clj
@@ -1,1 +1,9 @@
-(ns hospital.core)
+(ns hospital.core
+  (:require [clojure.test.check.generators :as gen]))
+
+(println (gen/sample gen/boolean 100))
+(println (gen/sample gen/int 100))
+(println (gen/sample gen/string))
+(println (gen/sample gen/string-alphanumeric 100))
+
+(println (gen/sample (gen/vector gen/int 5) 10))
