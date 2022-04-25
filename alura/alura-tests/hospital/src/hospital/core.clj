2dc1c01a01f3bc9287b44d91fa7f3fde791f7be2
Andre Macedo <dee.xd@hotmail.com>
2022-04-25 16:48:28 -0300

    feat: add generators e defspec

--- alura/alura-tests/hospital/src/hospital/core.clj
+++ alura/alura-tests/hospital/src/hospital/core.clj
@@ -1,9 +1,15 @@
 (ns hospital.core
   (:require [clojure.test.check.generators :as gen]))
 
-(println (gen/sample gen/boolean 100))
-(println (gen/sample gen/int 100))
-(println (gen/sample gen/string))
-(println (gen/sample gen/string-alphanumeric 100))
+(defn testing-gen
+  []
+  (println (gen/sample gen/boolean 100))
+  (println (gen/sample gen/int 100))
+  (println (gen/sample gen/string))
+  (println (gen/sample gen/string-alphanumeric 100))
 
-(println (gen/sample (gen/vector gen/int 5) 10))
+  (println (gen/sample (gen/vector gen/int 5) 10)))
+
+(defn greater-than-5?
+  [number]
+  (> number 5))
--- alura/alura-tests/hospital/test/hospital/core_test.clj
+++ alura/alura-tests/hospital/test/hospital/core_test.clj
@@ -1,3 +1,18 @@
 (ns hospital.core-test
   (:require [clojure.test :refer :all]
+            [clojure.test.check.clojure-test :refer (defspec)]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
             [hospital.core :refer :all]))
+
+(deftest greater-than-5?-test
+  (testing "random numbers are greater than 5"
+    (doseq [number (gen/sample gen/small-integer)]
+      (is (= (> number 5) (greater-than-5? number))))))
+
+(defspec exploring-api 10
+  (prop/for-all
+   [number (gen/vector gen/string-alphanumeric 0 4)
+    string gen/string-alphanumeric]
+   (println number string)
+   true))
