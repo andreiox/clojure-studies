4431c1424f74ba7cfa34628bdffb9f8352959499
Andre Macedo <dee.xd@hotmail.com>
2022-04-26 16:59:38 -0300

    feat: start project encoding-decoding

---
+++ simple-projects/encoding-decoding/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to encoding-decoding
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ simple-projects/encoding-decoding/test/encoding_decoding/core_test.clj
@@ -0,0 +1,7 @@
+(ns encoding-decoding.core-test
+  (:require [clojure.test :refer :all]
+            [encoding-decoding.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
